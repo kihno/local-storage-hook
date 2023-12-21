1b3950d1401ee80c9b960747f9ad87a4020b9387
kihno <stistyle13@gmail.com>
2023-12-20 16:43:15 -0800

    Implement local storage

--- src/use-local-storage.js
+++ src/use-local-storage.js
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
 export default function useLocalStorage(keyName, initialValue) {
-  // Add your solution here
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem(keyName)) || initialValue
+  );
+
+  const handleValueChange = (updatedValue) => {
+    localStorage.setItem(keyName, JSON.stringify(updatedValue));
+    setValue(updatedValue);
+  }
+
+  return [value, handleValueChange];
 }
